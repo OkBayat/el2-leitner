ab9eb0ae12ef554e075a842b46e77ce544da8ed0
Mohammad Bayat <OkBayat@gmail.com>
2026-09-01 11:03:32 +0330

    feat(ui): define required Material Web primitives

---
+++ ui/src/material-web.js
@@ -0,0 +1,5 @@
+import '@material/web/button/filled-button.js';
+import '@material/web/button/outlined-button.js';
+import '@material/web/textfield/outlined-text-field.js';
+import '@material/web/select/outlined-select.js';
+import '@material/web/select/select-option.js';
