7cb3e2a5596ebd14885c2587776b385610d134af
Mohammad Bayat <OkBayat@gmail.com>
2026-09-03 11:45:03 +0330

    security: allow Tatoeba audio delivery host

--- back/src/createApp.js
+++ back/src/createApp.js
@@ -91,7 +91,7 @@ export function createApp({
     helmet({
       contentSecurityPolicy: {
         directives: {
-          "media-src": ["'self'", "https://tatoeba.org"],
+          "media-src": ["'self'", "https://tatoeba.org", "https://audio.tatoeba.org"],
           // The documented Docker deployment is HTTP on localhost. Browsers
           // (notably Safari) may otherwise rewrite it to unavailable HTTPS.
           "upgrade-insecure-requests": null
