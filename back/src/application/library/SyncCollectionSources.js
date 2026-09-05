c25edbe2a8815109f02f077054136128399e5c47
Mohammad Bayat <OkBayat@gmail.com>
2026-09-05 12:49:36 +0330

    Defer collection source cleanup until after historical repairs

--- back/src/application/library/SyncCollectionSources.js
+++ back/src/application/library/SyncCollectionSources.js
@@ -11,14 +11,12 @@ export class SyncCollectionSources {
 
     const activeSlugs = sources.map((source) => source.slug);
     const archiveResult = await this.collectionSourceRepository.archiveMissing(activeSlugs);
-    const cleanupResult = await this.collectionSourceRepository.finalize();
 
     return {
       sourceCount: sources.length,
       changedCount: results.filter((result) => result.changed).length,
       archivedCount: archiveResult.archivedCount,
       archivedSlugs: archiveResult.archivedSlugs,
-      cleanup: cleanupResult,
       results
     };
   }
