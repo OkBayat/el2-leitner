13463fd9165a1d76be4f2d0d6aa7a36ae403ce42
Mohammad Bayat <OkBayat@gmail.com>
2026-09-06 18:46:42 +0330

    feat: add learning path resume query

---
+++ back/src/application/collection-learning-path/queries/GetLearningPathResumePoint.js
@@ -0,0 +1,29 @@
+import { findLearningPathResumePoint } from "../../../domain/collection-learning-path/LearningPathProgression.js";
+import {
+  ensureLearningPathReadAccess,
+  loadPathById,
+  projectedPathForUser,
+} from "../learningPathSupport.js";
+
+export class GetLearningPathResumePoint {
+  constructor({ definitionReader, progressReader, accessReader }) {
+    this.definitionReader = definitionReader;
+    this.progressReader = progressReader;
+    this.accessReader = accessReader;
+  }
+
+  async execute(userId, pathId) {
+    const path = await loadPathById(this.definitionReader, pathId);
+    await ensureLearningPathReadAccess(this.accessReader, userId, path);
+    const { projected } = await projectedPathForUser({
+      progressReader: this.progressReader,
+      userId,
+      path,
+    });
+    return {
+      pathId: projected.path.id,
+      pathStatus: projected.path.learnerStatus,
+      resumePoint: findLearningPathResumePoint(projected),
+    };
+  }
+}
