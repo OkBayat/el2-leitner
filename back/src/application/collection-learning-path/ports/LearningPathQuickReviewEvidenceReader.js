1691d1c8702686d23c5fbbbcd6a450227cb28e04
Mohammad Bayat <OkBayat@gmail.com>
2026-09-06 21:43:50 +0330

    feat(learning-path): define quick review evidence port

---
+++ back/src/application/collection-learning-path/ports/LearningPathQuickReviewEvidenceReader.js
@@ -0,0 +1,5 @@
+export class LearningPathQuickReviewEvidenceReader {
+  async findCompletedSession(_userId, _sessionId) {
+    throw new Error("LearningPathQuickReviewEvidenceReader.findCompletedSession must be implemented.");
+  }
+}
